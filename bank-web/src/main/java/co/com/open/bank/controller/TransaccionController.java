9af8c6814d2e50df55671022efa29414727db260
salas <salas@usbcali.edu.co>
2019-11-13 18:31:21 -0500

    Se añade impl de retirar(Transaccion)

--- bank-web/src/main/java/co/com/open/bank/controller/TransaccionController.java
+++ bank-web/src/main/java/co/com/open/bank/controller/TransaccionController.java
@@ -2,6 +2,7 @@ package co.com.open.bank.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.com.openintl.bank.dto.RetirarDTO;
+import co.com.openintl.bank.service.TransaccionService;
 
 @RestController
 @RequestMapping("/transaccion")
 public class TransaccionController {
 
+	@Autowired
+	private TransaccionService transaccionService;
+
 	@PostMapping("/retirar")
 	public ResponseEntity<?> retirar(@Valid @RequestBody RetirarDTO retirarDTO) {
-		return null;
+		try {
+			Long idTransaccionLong = transaccionService.retirar(retirarDTO.getCuenId(), retirarDTO.getValor(),
+					retirarDTO.getUsuUsuario());
+			return ResponseEntity.ok().body(idTransaccionLong);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(new ResponseError("400", e.getMessage()));
+
+		}
+
 	}
 
 }
