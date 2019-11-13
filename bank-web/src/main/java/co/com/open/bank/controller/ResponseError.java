22640ffd8b1855e7d051e1399cd2c9dfc15eb540
salas <salas@usbcali.edu.co>
2019-11-13 18:08:44 -0500

    Se añade clase para reponder con un error generico las execpciones controladas

---
+++ bank-web/src/main/java/co/com/open/bank/controller/ResponseError.java
@@ -0,0 +1,34 @@
+package co.com.open.bank.controller;
+
+public class ResponseError {
+
+	private String code;
+	private String message;
+
+	public ResponseError() {
+		super();
+	}
+
+	public ResponseError(String code, String message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
