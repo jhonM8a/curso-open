4551b56ab539292ecb3641841d8dae6a2cfe453e
salas <salas@usbcali.edu.co>
2019-11-13 18:17:47 -0500

    Se añade DTO para transacciones

---
+++ bank-logic/src/main/java/co/com/openintl/bank/dto/RetirarDTO.java
@@ -0,0 +1,57 @@
+package co.com.openintl.bank.dto;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+public class RetirarDTO {
+
+	@NotNull
+	@Size(min = 19, max = 19 ) 
+	private String cuenId;
+	@NotEmpty
+	@NotNull
+	private BigDecimal valor;
+	@NotEmpty
+	@NotNull
+	private String usuUsuario;
+
+	public RetirarDTO() {
+		super();
+	}
+
+	public RetirarDTO(String cuenId, BigDecimal valor, String usuUsuario) {
+		super();
+		this.cuenId = cuenId;
+		this.valor = valor;
+		this.usuUsuario = usuUsuario;
+	}
+
+	public String getCuenId() {
+		return cuenId;
+	}
+
+	public void setCuenId(String cuenId) {
+		this.cuenId = cuenId;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getUsuUsuario() {
+		return usuUsuario;
+	}
+
+	public void setUsuUsuario(String usuUsuario) {
+		this.usuUsuario = usuUsuario;
+	}
+
+}
