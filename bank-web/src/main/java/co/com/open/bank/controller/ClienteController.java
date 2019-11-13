3797917b3cde803024561c0e034899c758667172
salas <salas@usbcali.edu.co>
2019-11-12 21:00:27 -0500

    Se añde impl de delete en Controller de cliente

--- bank-web/src/main/java/co/com/open/bank/controller/ClienteController.java
+++ bank-web/src/main/java/co/com/open/bank/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,6 +72,17 @@ public class ClienteController {
 			cliente=clienteService.update(cliente);
 			clienteDTO=clienteMapper.entityToDTO(cliente);
 			return ResponseEntity.ok().body(clienteDTO);
+		}catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id){
+		
+		try {
+			clienteService.deleteById(id);
+			return ResponseEntity.ok("");
 		}catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
