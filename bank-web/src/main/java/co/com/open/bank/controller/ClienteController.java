618c15b53d5f6152f6c8a8aabb80e81f8770c637
salas <salas@usbcali.edu.co>
2019-11-13 18:02:28 -0500

    Se añade respusta por AOP de los badRequest

--- bank-web/src/main/java/co/com/open/bank/controller/ClienteController.java
+++ bank-web/src/main/java/co/com/open/bank/controller/ClienteController.java
@@ -1,17 +1,26 @@
 package co.com.open.bank.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.com.openintl.bank.domain.Cliente;
@@ -52,7 +61,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping("/save")
-	public ResponseEntity<?> save(@RequestBody ClienteDTO clienteDTO){
+	public ResponseEntity<?> save(@Valid @RequestBody ClienteDTO clienteDTO){
 		
 		try {
 			Cliente cliente = clienteMapper.dtoToEntity(clienteDTO);
@@ -65,7 +74,7 @@ public class ClienteController {
 	}
 	
 	@PutMapping("/update")
-	public ResponseEntity<?> update(@RequestBody ClienteDTO clienteDTO){
+	public ResponseEntity<?> update(@Valid @RequestBody ClienteDTO clienteDTO){
 		
 		try {
 			Cliente cliente = clienteMapper.dtoToEntity(clienteDTO);
@@ -86,5 +95,18 @@ public class ClienteController {
 		}catch (Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(
+	  MethodArgumentNotValidException ex) {
+	    Map<String, String> errors = new HashMap<>();
+	    ex.getBindingResult().getAllErrors().forEach((error) -> {
+	        String fieldName = ((FieldError) error).getField();
+	        String errorMessage = error.getDefaultMessage();
+	        errors.put(fieldName, errorMessage);
+	    });
+	    return errors;
 	}
 }
