bdd27d13f02377845810d0fa2faf2cd65daa0810
salas <salas@usbcali.edu.co>
2019-11-20 20:09:25 -0500

    Se modifican proyectos y se añade eureka y zuul

--- eureka-server/src/main/java/co/com/openintl/bank/EurekaServerApplication.java
+++ eureka-server/src/main/java/co/com/openintl/bank/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package co.com.openintl.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class BankClienteApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankClienteApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
